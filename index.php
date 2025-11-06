3491fcc2e1988d64355c4f7f58a2f8803a112be7
Dimas Yudha Prawira <prawira.dimas.yudha@gmail.com>
2025-11-06 12:20:12 +0700

    update lagi pesenan bos

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-echo "Selamat datang di Tokoroti versi 1.5 - via GitHub Actions<br>";
-
+echo "Selamat datang di Tokoroti versi 2.0 - via GitHub Actions<br>";
+echo "<br>";
+echo "<br>";
 echo "ini update terbaru<br>";
 ?>
