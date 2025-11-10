f14dfb6773e7c9c17fa7e2dd0d5a7d08c9520d5f
Dimas Yudha Prawira <prawira.dimas.yudha@gmail.com>
2025-11-10 11:47:40 +0700

    nambahin log

--- index.php
+++ index.php
@@ -3,4 +3,6 @@ echo "Selamat datang di Tokoroti versi 2.0 - via GitHub Actions<br>";
 echo "<br>";
 echo "<br>";
 echo "ini update terbaru<br>";
+echo "<br>";
+echo "ini branch tambahkan-log<br>";
 ?>
